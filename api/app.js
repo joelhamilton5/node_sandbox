1588aad1f2189e40818d11fc1880ef46ec4b68e4
Joel Hamilton <joel@mmddata.ca>
2020-02-25 15:55:01 -0500

    add static index page

--- api/app.js
+++ api/app.js
@@ -15,12 +15,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // assign route handlers
+var router = express.Router();
+app.use('/', router.get('/', function(req, res, next) {
+    res.send('working...');
+}));
+
 app.use('/socket', require('./components/socket/socket'));
 app.use('/users', require('./components/users/users'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    res.status(err.status || 404);
+app.use(function(err, req, res, next) {
+    res.status(404);
     res.send("404")
 });
 
@@ -28,13 +33,12 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
     console.log(err);
 
-
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(500);
     res.send("500")
 });
 
